fad0bf906fc826c53e19fcc86fdb73337fc4541c
Anthony Alayo <anthony.alayo@gmail.com>
2025-05-08 01:43:44 -0700

    actually building it

--- dist/index.js
+++ dist/index.js
@@ -7162,8 +7162,8 @@ async function $bd1d73aff0732146$var$injectCache(cacheSource, cacheOptions, scra
 FROM ${containerImage}
 COPY buildstamp buildstamp
 RUN --mount=${mountArgs} \
-    --mount=type=bind,source=.,target=/var/dance-cache \
-    cp -p -R /var/dance-cache/. ${targetPath} ${ownershipCommand} || true
+    --mount=type=bind,source=.,target=/var/dance-cache/${cacheSource} \
+    cp -p -R /var/dance-cache/${cacheSource}/. ${targetPath} ${ownershipCommand} || true
 `;
     await (0, $evV72$fspromises).writeFile((0, $evV72$path).join(scratchDir, 'Dancefile.inject'), dancefileContent);
     console.log(dancefileContent);
